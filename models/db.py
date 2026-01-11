9686909299e805c962126a7cd4c56e2f7419422f
Cassio Farias <cassiofarias@outlook.com>
2026-01-10 21:20:40 -0300

    Db to Env Variable

--- models/db.py
+++ models/db.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+db_name = os.getenv("DB_NAME") 
+db_user = os.getenv("DB_USER") 
 db_pass = os.getenv("DB_PASS") 
+db_host = os.getenv("DB_HOST") 
+db_port = os.getenv("DB_PORT") 
 
-db_dota = PostgresqlDatabase('dota_rank',
-                            user = 'postgres', 
+
+db_dota = PostgresqlDatabase(db_name,
+                            user = db_user, 
                             password = db_pass,
-                            host='localhost',
-                            port=5432 )
+                            host=db_host,
+                            port=db_port )
